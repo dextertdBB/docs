3d06a2f03476a1f268096ea7e4e4b9e4a3a52875
dextertd <dexterdickinson@protonmail.com>
2025-03-05 19:39:05 +0400

    Deploy website - based on 1b579570b61c85b406149300ebe3b166dbf3bc02

--- setup.sh
+++
@@ -1,2 +0,0 @@
-cp ./src/bybit_auth/buildPostmanRequest.js ./node_modules/docusaurus-theme-openapi-docs/lib-next/theme/ApiDemoPanel
-cp ./src/bybit_auth/crypto-js.min.js ./node_modules/docusaurus-theme-openapi-docs/lib-next/theme/ApiDemoPanel
